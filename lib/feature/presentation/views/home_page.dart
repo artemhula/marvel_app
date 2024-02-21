bfd94c5f926dfab33d09c12de475da865856e99d
artem <artemgul4@gmail.com>
2024-02-21 14:33:01 +0200

    added ScrollController for loading new characters

--- lib/feature/presentation/views/home_page.dart
+++ lib/feature/presentation/views/home_page.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marvel_app/feature/presentation/bloc/character_list_bloc/character_list_bloc.dart';
 import 'package:marvel_app/feature/presentation/views/search_page.dart';
 import 'package:marvel_app/feature/presentation/widgets/character_list_view.dart';
-
 
 enum Route { characters, comics }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({Key? key}) : super(key: key);
+
+  final _scrollController = ScrollController();
+
+  void _setupScrollController(BuildContext context) {
+    _scrollController.addListener(() {
+      if (_scrollController.position.maxScrollExtent ==
+          _scrollController.position.pixels) {
+        final state = context.read<CharacterListBloc>().state;
+        if (state is CharacterListLoaded) {
+          context.read<CharacterListBloc>().add(LoadCharacterList());
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    _setupScrollController(context);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -27,8 +43,10 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.only(right: 20),
             child: IconButton(
               onPressed: () {
-                Navigator.push(context,
-                    CupertinoPageRoute(builder: (context) => const SearchPage()));
+                Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                        builder: (context) => const SearchPage()));
               },
               icon: Padding(
                 padding: const EdgeInsets.only(right: 10),
@@ -65,7 +83,7 @@ class HomePage extends StatelessWidget {
               height: 10,
             ),
             Expanded(
-              child: CharacterListView(),
+              child: CharacterListView(scrollController: _scrollController),
             ),
           ],
         ),
--- lib/feature/presentation/widgets/character_list_view.dart
+++ lib/feature/presentation/widgets/character_list_view.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marvel_app/feature/presentation/bloc/character_list_bloc/character_list_bloc.dart';
@@ -7,7 +8,10 @@ import 'package:marvel_app/feature/presentation/widgets/try_again_button.dart';
 class CharacterListView extends StatelessWidget {
   const CharacterListView({
     super.key,
-  });
+    required ScrollController scrollController,
+  }) : _scrollController = scrollController;
+
+  final ScrollController _scrollController;
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +23,23 @@ class CharacterListView extends StatelessWidget {
         }
         if (state is CharacterListLoaded) {
           return ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              separatorBuilder: (context, index) => const SizedBox(
-                    height: 10,
-                  ),
-              itemCount: state.characters.length,
-              itemBuilder: (context, i) => CharacterCard(
+            controller: _scrollController,
+            physics: const BouncingScrollPhysics(),
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 10,
+            ),
+            itemCount: state.characters.length + 1,
+            itemBuilder: (context, i) => i < state.characters.length
+                ? CharacterCard(
                     name: state.characters[i].name,
                     description: state.characters[i].description,
                     image: state.characters[i].image,
-                  ));
+                  )
+                : const Padding(
+                    padding: EdgeInsets.only(top: 30, bottom: 30),
+                    child: Center(child: CircularProgressIndicator(color: Colors.red)),
+                  ),
+          );
         }
         if (state is CharacterListFailure) {
           return Center(
@@ -38,14 +49,15 @@ class CharacterListView extends StatelessWidget {
               const Text('Something went wrong'),
               TryAgainButton(
                 function: () {
-                  // sl<CharacterListBloc>().add(LoadCharacterList);
+                  context
+                      .read<CharacterListBloc>()
+                      .add(LoadCharacterList());
                 },
               ),
             ],
           ));
         }
-        return const Center(
-            child: CircularProgressIndicator(color: Colors.red));
+        return Container();
       },
     );
   }
