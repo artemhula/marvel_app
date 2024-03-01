26cda1578c55dd5740a8a53e054de000de4c4bd8
artem <artemgul4@gmail.com>
2024-03-01 17:31:57 +0200

    added new methods

--- lib/feature/data/datasources/character_local_data_source.dart
+++ lib/feature/data/datasources/character_local_data_source.dart
@@ -5,21 +5,27 @@ abstract class CharacterLocalDataSource {
   Future<void> charactersToCache(List<CharacterModel> characters);
   Future<List<CharacterModel>> getCharacters(int offset);
   Future<List<CharacterModel>> searchCharacters(String query);
+  Future<List<CharacterModel>> getFavoriteCharacters();
+  Future<bool> addFavoriteCharacter(CharacterModel character);
+  Future<bool> deleteFavoriteCharacter(int id);
+  Future<bool> checkFavoriteCharacter(int id);
 }
 
 class CharacterLocalDataSourceImpl implements CharacterLocalDataSource {
-  final Box<CharacterModel> _box;
+  final Box<CharacterModel> _characterListbox;
+  final Box<CharacterModel> _favoriteCharacterListbox;
 
-  CharacterLocalDataSourceImpl(this._box);
+  CharacterLocalDataSourceImpl(
+      this._characterListbox, this._favoriteCharacterListbox);
   @override
   Future<void> charactersToCache(List<CharacterModel> characters) async {
-    await _box.clear();
-    await _box.addAll(characters);
+    await _characterListbox.clear();
+    await _characterListbox.addAll(characters);
   }
 
   @override
   Future<List<CharacterModel>> getCharacters(int offset) async {
-    List<CharacterModel> characters = _box.values.toList();
+    List<CharacterModel> characters = _characterListbox.values.toList();
 
     final limit =
         offset + 20 > characters.length ? characters.length - offset : 20;
@@ -31,11 +37,36 @@ class CharacterLocalDataSourceImpl implements CharacterLocalDataSource {
 
   @override
   Future<List<CharacterModel>> searchCharacters(String query) async {
-    final List<CharacterModel> characters = _box.values
+    final List<CharacterModel> characters = _characterListbox.values
         .where((character) =>
             character.name.toLowerCase().contains(query.toLowerCase()))
         .toList();
 
     return characters;
+  }
+
+  @override
+  Future<List<CharacterModel>> getFavoriteCharacters() async {
+    if (_favoriteCharacterListbox.isEmpty) {
+      return [];
+    }
+    return _favoriteCharacterListbox.values.toList();
+  }
+
+  @override
+  Future<bool> checkFavoriteCharacter(int id) async {
+    return _favoriteCharacterListbox.containsKey(id);
+  }
+
+  @override
+  Future<bool> addFavoriteCharacter(CharacterModel character) async {
+    _favoriteCharacterListbox.put(character.id, character);
+    return true;
+  }
+
+  @override
+  Future<bool> deleteFavoriteCharacter(int id) async {
+    _favoriteCharacterListbox.delete(id);
+    return false;
   }
 }
