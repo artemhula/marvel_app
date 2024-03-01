c8e2f6905e343f541e3f9926e42d146c02707073
artem <artemgul4@gmail.com>
2024-03-01 17:30:06 +0200

    added new repositories

--- lib/feature/domain/repositories/character_repository.dart
+++ lib/feature/domain/repositories/character_repository.dart
@@ -5,4 +5,7 @@ import 'package:marvel_app/feature/domain/entities/character_entity.dart';
 abstract class CharacterRepository {
   Future<Either<Failure, List<CharacterEntity>>> getAllCharacters(int offset);
   Future<Either<Failure, List<CharacterEntity>>> searchCharacter(String query);
+  Future<Either<Failure, List<CharacterEntity>>> getFavoriteCharacters();
+  Future<Either<Failure, bool>> toggleFavoriteCharacter(CharacterEntity character);
+  Future<Either<Failure, bool>> checkFavoriteCharacter(int id);
 }
